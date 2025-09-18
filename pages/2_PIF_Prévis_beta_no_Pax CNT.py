6e24b258762a580c8b425f9dd8d1180f586a8709
cedadp <137264521+cedadp@users.noreply.github.com>
2025-09-18 16:44:29 +0200

    Update 2_PIF_Prévis_beta_no_Pax CNT.py

--- pages/2_PIF_Prévis_beta_no_Pax CNT.py
+++ pages/2_PIF_Prévis_beta_no_Pax CNT.py
@@ -96,13 +96,14 @@ if uploaded_file is not None:
     else:
         st.info("Fichier non-REPLAY chargé, aucun calcul spécifique appliqué")
 
-    if df is not None:
-                excel_data = download_excel(df)
-                st.download_button(
-                    label="📥 Télécharger le dataset modifié",
-                    data=excel_data,
-                    file_name=f'dataset_replay_{pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")}.xlsx',
-                    mim
+        if df is not None:
+            excel_data = download_excel(df)
+            st.download_button(
+            label="📥 Télécharger le dataset modifié",
+            data=excel_data,
+            file_name=f'dataset_replay_{pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")}.xlsx',
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    )
     
             
 
