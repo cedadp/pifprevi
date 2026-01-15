554e773c83c9d598527b22e6f977e3d53d5075e0
cedadp <137264521+cedadp@users.noreply.github.com>
2026-01-15 11:51:16 +0100

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,19 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="OutilsPIF V2", page_icon="🛫", layout="centered", initial_sidebar_state="auto")
+st.set_page_config(page_title="PIF Prévis", page_icon="🛫", layout="centered", initial_sidebar_state="auto")
 
 
-st.title('OutilsPIF V2') 
-
-st.write("Cet outil sert à regrouper plusieurs actions effectués au sein de l'IngeX de CDGD.")
-st.write("Vous retrouverez ainsi :")
-st.markdown("Onglet **📦 Concat** : Un outil de concaténation des programmes AF Skyteam et des programmes ADP.")
-st.markdown("Onglet **🛫 Pif Previ** : Un outil de prévisions des flux aux différents sites de PIF dans l'aéroport CDG.")
-st.markdown("Onglet **📊 Vérif Seuil PIF** : Un outil de visualisation des flux horaires aux différents sites de PIF dans l'aéroport CDG.")
-st.markdown("Onglet **✅ Export PIF** : Un outil de mise en forme des réalisés PIF.")
-st.markdown("Onglet **👩‍✈️ EquiPif** : Un outil de prévisions des flux PN aux différents sites de PIF dans l'aéroport CDG.")
-st.markdown("Onglet **📈 Dashboard** : Un outil de visualisation des flux aux différents sites de PIF.")
+st.title('PIF Prévis') 
 
 
 with st.sidebar.expander("Version"):
