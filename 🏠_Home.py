5ccdadfd6abd16aa0c8c30707231fd846814d789
cedadp <137264521+cedadp@users.noreply.github.com>
2026-01-22 16:15:11 +0100

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="PIF Prévis", page_icon="🛫", layout="centered", initial_sidebar_state="auto")
 
 
-st.title('PIF Prévis') 
+st.title('🛫 PIF Prévis') 
 
 
 with st.sidebar.expander("Version"):
