c14a1c143c8dc9cc72f9f32d9d9c4b88f3f945fd
cedadp <137264521+cedadp@users.noreply.github.com>
2026-07-29 10:53:34 +0200

    Update 3_Verif_Seuil_dev.py

--- pages/3_Verif_Seuil_dev.py
+++ pages/3_Verif_Seuil_dev.py
@@ -254,7 +254,7 @@ def main():
                     
         
 
-st.download_button ( "", 
+                    st.download_button ( "", 
                     data = df_depivote.to_csv(index=False).encode"utf-8-sig"), file_name = "data.csv", mime = "text/csv" )
        
 if __name__=="__main__":
