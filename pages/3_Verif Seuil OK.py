e3a2418266c4aa75d5056bc24c324125dd4987b2
cedadp <137264521+cedadp@users.noreply.github.com>
2026-09-10 15:26:47 +0200

    Update 3_Verif Seuil OK.py

--- pages/3_Verif Seuil OK.py
+++ pages/3_Verif Seuil OK.py
@@ -198,25 +198,25 @@ def main():
               
                 # df['jour']= pd.to_datetime(df['jour'])
                 
-                 for site in sites: 
+                for site in sites: 
                     
-                    st.subheader(f" {site}")
+                st.subheader(f" {site}")
                     
-                    st.write("seuil max: ", seuil(site))
-                    df_site = df_depivote.loc[df_depivote['site']==site]
-                    df_site['Date'] = df_site['jour'].apply(
-                    lambda d: f"{JOURS_FR[d.weekday()]} {d.day:02d} {MOIS_FR_ABR[d.month - 1]}"
+                st.write("seuil max: ", seuil(site))
+                df_site = df_depivote.loc[df_depivote['site']==site]
+                df_site['Date'] = df_site['jour'].apply(
+                lambda d: f"{JOURS_FR[d.weekday()]} {d.day:02d} {MOIS_FR_ABR[d.month - 1]}"
 )
                    
-                    fig = px.line(df_site, x= 'heure', y= 'charge',  color = 'Date',
+                fig = px.line(df_site, x= 'heure', y= 'charge',  color = 'Date',
                                     labels={'jour', 'date'})
                     
-                    ligne_seuil = seuil(site)
-                    fig.add_hline(y=ligne_seuil, line_dash='dash', line_color="red")
+                ligne_seuil = seuil(site)
+                fig.add_hline(y=ligne_seuil, line_dash='dash', line_color="red")
                     
                     
                     
-                    st.plotly_chart(fig)
+                st.plotly_chart(fig)
     
         
   
